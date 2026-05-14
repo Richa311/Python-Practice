5313cf11806623486cead6d2d5ad05ca444d1dec
Richa311 <patelri@wsc.school.nz>
2026-05-14 12:18:08 +1200

    Created a quiz on which animal are you ?

---
+++ Lesson 5/which_animal.py
@@ -0,0 +1,23 @@
+answer = input(" Do you like adventure or Travelling?").strip()
+if answer == "Adventure": 
+    answer = input (' Are you very active or energetic? ')
+    if answer == 'Active': 
+        print (" You are Owl")
+    else : 
+       print (" You are Tiger")
+else : 
+    answer = input (' Are you a people person?')
+    if answer == "Yes" :
+        print ('you are seal ')
+    else:  
+        print(" You are hippo") 
+    
+    
+    
+#     print (" Do you prefer group activities?")
+# if energatic
+# if answer == 'Yes':  
+    
+# if answer == 'No': 
+    
+#     print (" Love doing things in Large Crowd? ")
