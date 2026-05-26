30428a0c1458a2bb72932c9c6c9b0428eba830e3
Richa311 <patelri@wsc.school.nz>
2026-05-26 12:23:25 +1200

    working on int and str, statments

--- _main.py
+++ _main.py
@@ -1,1 +1,18 @@
+# --- THE GAMER STATS CALCULATOR ---
 
+# Section 1
+user_input = int(input("Enter your age: ")) 
+birth_year = 2026 - user_input
+age_in_2000= 2000 - user_input 
+print("You were () years old in the year 2000!")
+
+# Section 2
+spawn_rate = "5"
+multiplier = 3
+total_enemies = int(spawn_rate * multiplier)
+print("Total enemies spawning: " + str(total_enemies))
+
+
+# Section 3
+score = 950
+print("Your high score is: " + score)
