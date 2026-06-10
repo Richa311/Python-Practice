f6825f84d5dd0fa597f2b7e55f44bb783b6cc491
Richa311 <patelri@wsc.school.nz>
2026-06-11 11:52:52 +1200

    started making a random-pokemon game

--- quiz.py
+++ quiz.py
@@ -69,7 +69,7 @@ if ans9 ==" 18" or ans9.lower == "eighteen":
 else:
     print (" Wrong. Argon have 18 electrons. ")
 ans10= int(input("What is the atomic number of Argon?").strip ().upper())
-if ans10==" 16" or " Sixteen":  #need to debug #
+if ans10=="16" or ans10== " sixteen ":
     print(" Correct answer!")
     score+=1
 else: 
